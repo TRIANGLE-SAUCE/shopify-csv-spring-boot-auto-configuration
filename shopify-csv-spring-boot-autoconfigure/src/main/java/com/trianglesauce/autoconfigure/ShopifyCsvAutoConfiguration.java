72c1be1d512baae6937253bda401e91275f853da
Ryosuke <ry0suke.uch1tate@gmail.com>
2020-08-25 00:45:47 +0900

    Separate module

--- shopify-csv-spring-boot-autoconfigure/src/main/java/com/trianglesauce/autoconfigure/ShopifyCsvAutoConfiguration.java
+++ shopify-csv-spring-boot-autoconfigure/src/main/java/com/trianglesauce/autoconfigure/ShopifyCsvAutoConfiguration.java
@@ -1,11 +1,13 @@
-package com.trianglesauce.shopifycsvspringbootstarter.autoconfigure;
+package com.trianglesauce.autoconfigure;
 
 import com.trianglesauce.service.CustomerService;
 import com.trianglesauce.service.impl.CustomerServiceImpl;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnBean(CustomerService.class)
 public class ShopifyCsvAutoConfiguration {
 
 	@Bean
