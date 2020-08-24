a664f3e0edfdb8fc8459d609c7210312662e38b3
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-08-25 02:56:10 +0900

    Fix bug (#12)
    * Fix bug
    
    * Fix version

--- shopify-csv-spring-boot-autoconfigure/src/main/java/com/trianglesauce/autoconfigure/ShopifyCsvAutoConfiguration.java
+++ shopify-csv-spring-boot-autoconfigure/src/main/java/com/trianglesauce/autoconfigure/ShopifyCsvAutoConfiguration.java
@@ -2,14 +2,14 @@ package com.trianglesauce.autoconfigure;
 
 import com.trianglesauce.service.CustomerService;
 import com.trianglesauce.service.impl.CustomerServiceImpl;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnBean(CustomerService.class)
 public class ShopifyCsvAutoConfiguration {
 
+	@ConditionalOnMissingBean(CustomerService.class)
 	@Bean
 	public CustomerService customerService() {
 		return new CustomerServiceImpl();
